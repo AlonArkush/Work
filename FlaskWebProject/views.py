53db3237becdc64a8af940e55a20c49e7b40110a
Alon <alon23.arkush@gmail.com>
2020-04-20 19:20:22 +0300

    added today's things

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -7,10 +7,43 @@ from flask import render_template,request, Response, jsonify
 from FlaskWebProject import app, socketio
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+import random
+import sqlite3
 
 
 
+def get_sentences():
+    cursor = conn.execute('SELECT * from sentences')
+    rows = cursor.fetchall()
+    row = random.choice(rows)
+    return row
+conn = sqlite3.connect('example.db')
+print ("Opened Database succesfully")
+s = get_sentences()
+print (s)
+the_sentence = s[1]
 
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS sentences(number, sentence)')
+
+def data_entry():
+    c.execute('INSERT INTO sentences VALUES(1, "He put heat on the wound to see what would grow.")')
+    c.execute('INSERT INTO sentences VALUES(2, "He found the end of the rainbow and was surprised at what he found there.")')
+    c.execute('INSERT INTO sentences VALUES(3, "Be careful with that butter knife.")')
+    c.execute('INSERT INTO sentences VALUES(4, "I was very proud of my nickname throughout high school but today- I couldn’t be any different to what my nickname was.")')
+    c.execute('INSERT INTO sentences VALUES(5, "Each person who knows you has a different perception of who you are.")')
+    c.execute('INSERT INTO sentences VALUES(6, "Beach-combing replaced wine tasting as his new obsession.")')
+    c.execute('INSERT INTO sentences VALUES(7, "She tilted her head back and let whip cream stream into her mouth while taking a bath.")')
+    c.execute('INSERT INTO sentences VALUES(8, "The pet shop stocks everything you need to keep your anaconda happy.")')
+    c.execute('INSERT INTO sentences VALUES(9, "Seek success, but always be prepared for random cats.")')
+    c.execute('INSERT INTO sentences VALUES(10, "Writing a list of random sentences is harder than I initially thought it would be.")')
+    conn.commit()
+    c.close()
+    conn.close()
+
+#create_table()
+#data_entry()
 
 @app.route('/')
 def index():
@@ -56,12 +89,10 @@ def add3():
     )
 
 
-
-
 @app.route('/add4', methods=[ 'GET','POST'])
 def add4():
     print("2222")
-    if request.method == 'POST':       
+    if request.method == 'POST':
         value1 = request.form.get('yosi')
         value1 = int(value1)
         value2 = request.form.get('moshe')
@@ -111,9 +142,6 @@ def add6():
         title='Home Page',
         year=datetime.now().year,
     )
-
-
-
         
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')    
@@ -131,21 +159,17 @@ def handle_my_custom_event(data, methods=['GET', 'POST']):
         
 
 
+     
 
 
-
-
-
-        
-
-
-        
+            
 @app.route('/add7', methods=[ 'GET'])
 def add7():
     print("add7")
     return render_template(
         'add7.html',
         title='Home Page',
+        theText = the_sentence,
         year=datetime.now().year,
     )
 
@@ -154,7 +178,15 @@ def add7():
 def handle_keypress_event(data, methods=['GET', 'POST']):
     print("1243",data)
     if "val1" in data :
-        val1 = data["val1"]
-        data = {"val3":val1}
+        val1 = data["val1"] #get the word
+        l = len(val1) #get the length of the word
+        theText = the_sentence #the word
+        w = theText[:l] #shortened word
+        val4 = ""
+        if val1 != w:
+            val4="red"
+        if val1 == w:
+            val4="black"
+        data = {"val3":val1,"val4":val4}
         socketio.emit('keypress_response', data, callback=messageReceived)        
         
